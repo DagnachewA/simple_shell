06ffdeac2f1d9af9c95fed7e996bfb1ac98dd488
kimkelvin7 <kelvinkimm70@gmail.com>
2023-12-12 11:13:49 -0800

    builtins

--- builtins.c
+++ builtins.c
@@ -8,8 +8,8 @@ int sh_help(char **args, char __attribute__((__unused__)) **front);
 /**
  * get_builtin - Matches a command with a corresponding
  *               sh builtin function.
- * @command: Command to match.
- * Return: A function pointer to the corresponding builtin.
+ * @command: The command to match.
+ * Return: A function pointer to corresponding builtin.
  */
 
 int (*get_builtin(char *command))(char **args, char **front)
